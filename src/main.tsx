bea7db84fe3e52714b87e39439adc8ffe080c195
advenslk <nethummenura198@gmail.com>
2026-08-27 10:07:20 +0530

    Wait for CMS hydration before rendering app

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const cmsReady = (window as Window & { ArveXCMSReady?: Promise<unknown> }).ArveXCMSReady;
+
+const renderApp = () => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+};
+
+if (cmsReady && typeof cmsReady.then === 'function') {
+  cmsReady.then(renderApp).catch(renderApp);
+} else {
+  renderApp();
+}
